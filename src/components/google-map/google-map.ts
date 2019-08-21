431a8f234a82bd948096fdaee48449b9fe0f6f6c
449sharon <50619633+449sharon@users.noreply.github.com>
2019-08-21 14:10:07 +0200

    problemu

--- src/components/google-map/google-map.ts
+++ src/components/google-map/google-map.ts
@@ -2,6 +2,7 @@
 import { Component, ViewChild } from '@angular/core';
 
 
+
 @Component({
   selector: 'google-map',
   templateUrl: 'google-map.html'
@@ -19,24 +20,19 @@ ngOnInit(){
   this.initMap();
  // console.log('wffergfefe');
 }
-  initMap(){
-
-    let coords = new google.maps.LatLng(-12,12);
-    let mapOptions: google.maps.MapOptions = {
-      center: coords,
-      zoom: 11,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions)
-
-
-    let marker: google.maps.Marker = new google.maps.Marker({
-      map: this.map,
-      position: coords
-    })
-
+initMap(){
+  let coords = new google.maps.LatLng(-22.777, 23.533)
+  let mapOptions: google.maps.MapOptions = {
+    center: coords,
+    zoom: 11,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   }
+  this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions)
+  let marker: google.maps.Marker = new google.maps.Marker({
+    map: this.map,
+    position: coords
+  })
+}
 
 
 }
