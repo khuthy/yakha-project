d7358dc40b53b22e0ab697e8755eb746eccc65a5
449sharon <50619633+449sharon@users.noreply.github.com>
2019-08-26 09:24:45 +0200

    updated changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -39,7 +39,7 @@ export class HomePage {
 
 //viewmore
 viewBuilderInfo(){
-  this.modalCtrl.create(BuilderProfileviewPage).present();
+  this.navCtrl.push(BuilderProfileviewPage);
 }
 
 // onLocateUser(){
