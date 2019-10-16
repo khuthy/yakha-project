b5e93c2477c33a7eae6d9457a1389afb656db888
449sharon <50619633+449sharon@users.noreply.github.com>
2019-10-16 11:54:13 +0200

    updates

--- src/pages/channels/channels.ts
+++ src/pages/channels/channels.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { MessagesPage } from '../messages/messages';
+import * as firebase from 'firebase'
 
 /**
  * Generated class for the ChannelsPage page.
@@ -15,15 +16,48 @@ import { MessagesPage } from '../messages/messages';
   templateUrl: 'channels.html',
 })
 export class ChannelsPage {
-
+  db = firebase.firestore();
+  uid = firebase.auth().currentUser.uid;
+  dat = {} as builderProfile;
+  builder;
+  respond = [];
+  user;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+   /*  this.dat = this.navParams.data; */
+   this.db.collection('Respond').where('hOwnerUID', '==', this.uid).onSnapshot((response) => {
+      response.forEach(doc => {
+        this.db.collection('Users').where('uid', '==', doc.data().uid).onSnapshot((userDetails) => {
+          userDetails.forEach((users) => {
+            this.respond.push({user: users.data(), autoId: doc.id});
+          })
+        })
+       
+      });
+   })
+  
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChannelsPage');
+   
   }
-  gotoMessages() {
-    this.navCtrl.push(MessagesPage);
+  gotoMessages(autoUid) {
+  this.navCtrl.push(MessagesPage , autoUid);
+  console.log(autoUid);
+  
   }
+  
 
+}
+export interface builderProfile {
+  uid: '',
+  image: '',
+  fullName: '',
+  certified: false,
+  experiences: '',
+  address: '',
+  price: '',
+  location: '',
+  roof: '',
+  gender: ''
 }
--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -22,7 +22,7 @@ import { PopoverPage } from '../popover/popover';
   templateUrl: 'messages.html',
 })
 export class MessagesPage {
-
+ db = firebase.firestore();
   dbMessage = firebase.firestore().collection('Request');
   dbIncoming = firebase.firestore().collection('Respond');
   dbProfile = firebase.firestore().collection('Users');
@@ -35,7 +35,23 @@ export class MessagesPage {
   honwerUID;
   hownerName;
   homebuilder: boolean; //testing if the css is working
+  
   msg: any;
+  /* testing */
+  autoUid: any;
+  homeOwner: any;
+  homeDetails: any;
+  homeUid;
+  projectRequirement = {
+    brickType: '',
+    wallType: '',
+    date: '',
+    comment: '',
+    startDate: '',
+    endDate: '',
+    extras: []
+
+  }
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
       private fileOpener: FileOpener,
@@ -44,6 +60,10 @@ export class MessagesPage {
       public authServes: AuthServiceProvider,
       public oneSignal: OneSignal, public popoverCtrl: PopoverController
       ) {
+
+        this.autoUid = this.navParams.data;
+        console.log(this.autoUid);
+        
  
   }
   acceptQoute(data, uid){
@@ -116,7 +136,21 @@ export class MessagesPage {
     })
    /*  document.getElementById('accept').style.display="none";
     document.getElementById('review').style.display="none"; */
+
   })
+  /* get request */
+   this.db.collection('Request').doc(this.autoUid).onSnapshot((getRequest) => {
+          if(getRequest.exists){
+            this.projectRequirement.brickType = getRequest.data().brickType;
+            this.projectRequirement.startDate = getRequest.data().startDate;
+            this.projectRequirement.wallType = getRequest.data().wallType;
+            this.projectRequirement.comment = getRequest.data().comment;
+            this.projectRequirement.endDate = getRequest.data().endDate;
+            this. projectRequirement.date = getRequest.data().date;
+          }
+     
+   })
+  
   }
  
   presentPopover(uid) {
