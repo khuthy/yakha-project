ef7b7eda0c9c7591a315e6a7075642b42a208e74
449sharon <50619633+449sharon@users.noreply.github.com>
2019-10-18 15:54:52 +0200

    updates

--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -78,7 +78,7 @@ export class MessagesPage {
     
   }
 
-  open() {
+  open(){
     if (this.toggle == true) {
       this.toggle = false;
       this.icon = 'arrow-dropdown';
@@ -155,10 +155,10 @@ export class MessagesPage {
       })
       console.log('Response message....', this.incomingRes );
     })
-    // this.dbMessage.doc(this.uid).onSnapshot((res)=>{
-    //   this.msgSent.push(res.data());
-    //   console.log('Message sent>>>>',this.msgSent);
-    // })
+    this.dbMessage.doc(this.uid).onSnapshot((res)=>{
+      this.msgSent.push(res.data());
+      console.log('Message sent>>>>',this.msgSent);
+    })
    // this.messages.push(data);
     // this.dbMessage.doc(this.autoUid.id).onSnapshot((res) => {
     //   data.sent = res.data();
